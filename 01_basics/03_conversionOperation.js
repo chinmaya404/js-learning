8845578855a43a82d559078a6eac0998051fa5b3
Chinmaya Behera <beherachinmaya786@gmail.com>
2026-06-25 18:27:10 +0000

    datatype conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,56 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+// ****************************  Operations  *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " chinmaya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 & 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+// -----------------------------
+let x = 3;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
